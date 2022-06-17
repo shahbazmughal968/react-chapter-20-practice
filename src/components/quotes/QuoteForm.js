88a725b5d24f8fdd26705bf24ea37c64f4fb7128
Shahbaz Mughal <shahbaz.mughal@RWP.ORCALO.LOCAL>
2022-06-17 23:59:36 +0500

    #React chapter-20 practice video video 20

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,5 +1,6 @@
-import { useRef } from 'react';
-
+import { useRef,useState } from 'react';
+import { Prompt } from 'react-router-dom';
+import { Fragment } from 'react/cjs/react.production.min';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
@@ -7,7 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
-
+const [isEntering,setIsEntering]=useState(false);
   function submitFormHandler(event) {
     event.preventDefault();
 
@@ -19,9 +20,17 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const formFocusedHandler =()=>{
+    setIsEntering(true);
+  }
+  const finishEnteringHandler = () =>{
+    setIsEntering(false)
+  }
   return (
+    <Fragment>
+      <Prompt when={isEntering } message={(location)=>'Are you sure you want to leave? All your entered data will be lost!'}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +46,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishEnteringHandler}  className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,8 +1,12 @@
 import QuoteForm from '../components/quotes/QuoteForm'
-
+import { useHistory } from 'react-router-dom'
 const NewQuote = () =>{
+
+    const history=useHistory();
+
     const addQuotesHandler = quoteData =>{
         console.log(quoteData)
+        history.push('/quotes')
     }
     return(
         <QuoteForm onAddQuote={addQuotesHandler}/>
